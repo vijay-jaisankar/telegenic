47135aa4dbb4ffad3aa4ded81bbceb37ec737427
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-02-19 17:39:33 +0530

    feat: add moviepy-native filters

--- tool/video-filters-service/filters.py
+++ tool/video-filters-service/filters.py
@@ -3,63 +3,103 @@
 """
 import cv2
 from moviepy.editor import VideoFileClip
-from moviepy.video import fx
-
 from moviepy.decorators import apply_to_audio, apply_to_mask
 
 
-def speedx(clip, factor = None, final_duration=15.0):
-    """
+def speedx(clip, factor=None, final_duration=15.0):
+  """
     Returns a clip playing the current clip but at a speed multiplied
     by ``factor``. Instead of factor one can indicate the desired
     ``final_duration`` of the clip, and the factor will be automatically
     computed.
     The same effect is applied to the clip's audio and mask if any.
     """
-    
-    if final_duration:
-        factor = 1.0* clip.duration / final_duration
-        
-    newclip = clip.fl_time(lambda t: factor * t, apply_to=['mask', 'audio'])
-    
-    if clip.duration is not None:
-        newclip = newclip.set_duration(1.0 * clip.duration / factor)
-    
-    return newclip
+
+  if final_duration:
+    factor = 1.0 * clip.duration / final_duration
+
+  newclip = clip.fl_time(lambda t: factor * t, apply_to=['mask', 'audio'])
+
+  if clip.duration is not None:
+    newclip = newclip.set_duration(1.0 * clip.duration / factor)
+
+  return newclip
+
 
 def apply_blur(image):
-    return cv2.medianBlur(image,5)
+  return cv2.medianBlur(image, 5)
 
 def apply_brightness(image):
-    return cv2.convertScaleAbs(image, alpha=2.0, beta=0.1)
+  return cv2.convertScaleAbs(image, alpha=2.0, beta=0.1)
 
+
+def invert_green_blue(image):
+  return image[:, :, [0, 2, 1]]
+
+
+def scroll(get_frame, t):
+  frame = get_frame(t)
+  frame_region = frame[int(t):int(t) + 360, :]
+  return frame_region
 
 
 """
     Light Blur filter
 """
+
 def apply_blur_filter(input_filename, output_filename):
-    try:
-        clip = VideoFileClip(input_filename)
-        new_clip = clip.fl_image(apply_blur)
-        new_clip_scaled = speedx(new_clip)
-        new_clip_scaled.write_videofile(output_filename)
+  try:
+    clip = VideoFileClip(input_filename)
+    new_clip = clip.fl_image(apply_blur)
+    new_clip_scaled = speedx(new_clip)
+    new_clip_scaled.write_videofile(output_filename)
 
-    except Exception as e:
-        print(f"Error while processing the blur filter: {e.with_traceback()}")
-        return None
-    
-
+  except Exception as e:
+    print(f"Error while processing the blur filter: {e.with_traceback()}")
+    return None
 """
     Bright filter
 """
-def apply_bright_filter(input_filename, output_filename):
-    try:
-        clip = VideoFileClip(input_filename)
-        new_clip = clip.fl_image(apply_brightness)
-        new_clip_scaled = speedx(new_clip)
-        new_clip_scaled.write_videofile(output_filename)
 
-    except Exception as e:
-        print(f"Error while processing the bright filter: {e.with_traceback()}")
-        return None
+def apply_bright_filter(input_filename, output_filename):
+  try:
+    clip = VideoFileClip(input_filename)
+    new_clip = clip.fl_image(apply_brightness)
+    new_clip_scaled = speedx(new_clip)
+    new_clip_scaled.write_videofile(output_filename)
+
+  except Exception as e:
+    print(f"Error while processing the bright filter: {e.with_traceback()}")
+    return None
+"""
+  Colour changer filter
+"""
+
+
+def apply_colour_change_filter(input_filename, output_filename):
+  try:
+    clip = VideoFileClip(input_filename)
+    new_clip = clip.fl_image(invert_green_blue)
+    new_clip_scaled = speedx(new_clip)
+    new_clip_scaled.write_videofile(output_filename)
+
+  except Exception as e:
+    print(f"Error while processing the bright filter: {e.with_traceback()}")
+    return None
+
+
+"""
+  Scroll filter
+"""
+
+
+def apply_scroll_filter(input_filename, output_filename):
+  try:
+    clip = VideoFileClip(input_filename)
+    new_clip = clip.fl(scroll)
+    new_clip_scaled = speedx(new_clip)
+    new_clip_scaled.write_videofile(output_filename)
+
+  except Exception as e:
+    print(f"Error while processing the bright filter: {e.with_traceback()}")
+    return None
