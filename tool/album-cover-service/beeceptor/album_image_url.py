5439d507132fa9c83290caeab60adb67a596d762
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-02-19 15:07:06 +0530

    bugfix: add failsafe imgur string ref: #4

--- tool/album-cover-service/beeceptor/album_image_url.py
+++ tool/album-cover-service/beeceptor/album_image_url.py
@@ -1,23 +1,22 @@
 import random
 import requests
-
 """
     Generate URL using the deployed Beeceptor endpoint
 """
-def get_image_url(type_param : int) -> str:
+def get_image_url(type_param: int) -> str:
     url = f"https://telegenic.free.beeceptor.com/album/type{type_param}"
     return url
+
 
 """
     Get a prospective album cover URL
 """
 def get_url_from_beeceptor() -> str:
-    type_param = random.randint(1,2)
-    url = get_image_url(type_param)
-    r = requests.get(url)
-    return (r.json()["status"])
-
-
-if __name__ == "__main__":
-    s = get_url_from_beeceptor()
-    print(s)
+    try:
+        type_param = random.randint(1, 2)
+        url = get_image_url(type_param)
+        r = requests.get(url)
+        return (r.json()["status"])
+    except Exception as e:
+        print(f"Error while getting album cover link from Beeceptor: {e}")
+        return "https://i.imgur.com/oA0yD7d.png"
